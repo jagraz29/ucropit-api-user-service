07b8e56b80e4ca65c8a4bba72b577f11b6637aad
Lucas Michailian <lucasmichailian@gmail.com>
2019-02-06 09:33:35 -0300

    hash password

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcrypt')
+const { pick } = require('lodash')
 const { User, validate } = require('../models/users')
 const mongoose = require('mongoose')
 const express = require('express')
@@ -16,20 +18,20 @@ router.post('/', async (req, res) => {
   try {
     const { error } = validate(req.body)
     if (error) return res.status(400).send(error.details[0].message)
-    
+
     let user = await User.findOne({ email: req.body.email })
     if (user) return res.status(400).send('User already registered')
 
-    user = new User({
-      firstname: req.body.firstname,
-      lastname: req.body.lastname,
-      email: req.body.email,
-      password: req.body.password
-    })
-
+    user = new User(pick(req.body, ['firstname', 'lastname', 'email', 'password', 'phone', 'cuit']))
+    const salt = await bcrypt.genSalt(10)
+    user.password = await bcrypt.hash(user.password, salt)
+    
     await user.save()
+    
+    res.send(
+      pick(user, ['_id', 'firstname', 'lastname', 'email', 'cuit', 'phone'])
+    )
 
-    res.send(user)
   } catch (err) {
     res.status(500).send(err)
   }
