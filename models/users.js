98bb34a36ed807ba61b74d544569b292525cee4a
Lucas Michailian <lucasmichailian@gmail.com>
2019-02-06 10:18:27 -0300

    add jwt library and validation

--- models/users.js
+++ models/users.js
@@ -42,7 +42,7 @@ function validateUser(user) {
     firstname: Joi.string().max(50).required(),
     lastname: Joi.string().max(50).required(),
     email: Joi.string().min(5).max(255).required().email(),
-    password: Joi.string().min(5).max(1024).required()
+    password: Joi.string().min(6).max(1024).required()
   }
 
   return Joi.validate(user, schema)
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,37 @@
 const bcrypt = require('bcrypt')
-const { pick } = require('lodash')
-const { User, validate } = require('../models/users')
+const jwt = require('jsonwebtoken')
+const { omit } = require('lodash')
+const { User } = require('../models/users')
+const Joi = require('joi')
 const mongoose = require('mongoose')
 const express = require('express')
 const router = express.Router()
+
+router.post('/', async (req, res) => {
+  const { error } = validate(req.body)
+  if (error) return res.status(422).send(error.details[0].message)
+
+  let user = await User.findOne({ email: req.body.email })
+  if (!user) return res.status(401).send('Invalid email or password')
+
+  const invalidPassword = await bcrypt.compare(req.body.password, user.password)
+  if (!invalidPassword) return res.status(401).send('Invalid email or password')
+
+  const token = jwt.sign({ _id: user._id }, 'theloveofthegod')
+
+  res.send({
+    user: omit(user, ['password']),
+    token
+  })
+})
+
+function validate(req) {
+  const schema = {
+    email: Joi.string().max(255).required().email(),
+    password: Joi.string().min(6).max(1024).required()
+  }
+
+  return Joi.validate(req, schema)
+}
+
+module.exports = router
