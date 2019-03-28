9e6af3e52b3a5cad0cb780d48bda515013db9e13
Lucas Michailian <lucasmichailian@gmail.com>
2019-03-28 17:02:28 -0300

    add dependencies

--- startup/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-module.exports = function () {
-  mongoose.connect(process.env.MONGO_CONNECTION, {
-    useNewUrlParser: true,
-    useCreateIndex: true
-  }).then(() => console.log('Connected to MongoDB'))
-}
