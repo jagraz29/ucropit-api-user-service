5771da80a763b06dd14e66cfc594f42c8ee2c8c3
Lucas Michailian <lucasmichailian@gmail.com>
2019-05-09 09:25:25 -0300

    change minmax crop types seeder

--- src/seeders/20190407140838-crop_types.js
+++ src/seeders/20190407140838-crop_types.js
@@ -5,32 +5,32 @@ module.exports = {
     return queryInterface.bulkInsert('crop_types', [
       {
         name: 'Algodón',
-        max_tons: 1,
-        min_tons: 3,
+        max_tons: 30,
+        min_tons: 10,
         image: 'cotton.png',
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         name: 'Girasol',
-        max_tons: 1,
-        min_tons: 3,
+        max_tons: 40,
+        min_tons: 10,
         image: 'sunflower.png',
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         name: 'Soja',
-        max_tons: 1,
-        min_tons: 3,
+        max_tons: 50,
+        min_tons: 10,
         image: 'soy.png',
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         name: 'Maíz Pisingallo',
-        max_tons: 1,
-        min_tons: 3,
+        max_tons: 60,
+        min_tons: 10,
         image: 'corn.png',
         createdAt: new Date(),
         updatedAt: new Date()
