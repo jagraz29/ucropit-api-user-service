32ca8c6f902f7d30820827c051134b6f6f174ba5
Samuel Garcia <rsamuelgarcia@gmail.com>
2019-06-19 20:57:03 -0400

    feature upload service

---
+++ src/services/UploadFiles.js
@@ -0,0 +1,33 @@
+function filterFile(req, file, cp) {
+  const fileType = file.originalname.split('.').pop()
+  if(file.fieldname === 'image'){
+    switch (fileType) {
+      case 'jpg':
+        cp(null, true)
+        break
+      case 'png':
+        cp(null, true)
+        break
+      default:
+        cp(null, false)
+        break
+    }
+  }else{
+    switch (fileType) {
+      case 'pdf':
+        cp(null, true)
+        break
+      case 'docx':
+        cp(null, true)
+        break
+      case 'doc':
+        cp(null, true)
+        break
+      default:
+        cp(null, false)
+        break
+    }
+  }
+  cp(null, false)
+}
+module.exports = filterFile
