17c3071eb0c71abe93a9ac22a4d06689b0063553
Franco Correa <anibalfranciscocorrea@gmail.com>
2019-07-03 12:46:12 -0300

    Busqueda de tipos de insumos por name

--- src/controllers/ConceptsController.js
+++ src/controllers/ConceptsController.js
@@ -1,47 +1,68 @@
-'use strict'
+"use strict";
 
 const Services = require("../models").services;
 const Inputs = require("../models").inputs;
 const InputType = require("../models").input_types;
 const ServiceType = require("../models").service_types;
+const Sequelize = require("sequelize");
 
 class ConceptsController {
   static async index() {
     try {
-      const inputs = await Inputs.findAll({ include: [{ model: InputType }] })
-      const services = await Services.findAll({ include: [{ model: ServiceType }] })
+      const inputs = await Inputs.findAll({ include: [{ model: InputType }] });
+      const services = await Services.findAll({
+        include: [{ model: ServiceType }]
+      });
 
       return Promise.resolve([
         ...services.map(el => {
-          const plain = el.get({ plain: true })
-          plain.category = 'service'
-          return plain
+          const plain = el.get({ plain: true });
+          plain.category = "service";
+          return plain;
         }),
         ...inputs.map(el => {
-          const plain = el.get({ plain: true })
-          plain.category = 'input'
-          return plain
+          const plain = el.get({ plain: true });
+          plain.category = "input";
+          return plain;
         })
-      ])
+      ]);
     } catch (e) {
-      throw new Error(e)
+      throw new Error(e);
     }
   }
 
   static async show(id, type) {
-    console.log(id, type === 'service')
     try {
-      if (type === 'service') {
-        const input = await Services.findOne({ where: { id }, include: [{ model: ServiceType }] })
-        return input
+      if (type === "service") {
+        const input = await Services.findOne({
+          where: { id },
+          include: [{ model: ServiceType }]
+        });
+        return input;
       } else {
-        const services = await Inputs.findOne({ where: { id }, include: [{ model: InputType }] })
-        return services
+        const services = await Inputs.findOne({
+          where: { id },
+          include: [{ model: InputType }]
+        });
+        return services;
       }
     } catch (e) {
-      throw new Error(e)
+      throw new Error(e);
+    }
+  }
+
+  static async searchInpuType(query) {
+    const Op = Sequelize.Op;
+    try {
+      const results = await InputType.findAll({
+        where: { name: { [Op.like]: `%${query}%` } }
+      });
+
+      return results;
+    } catch (e) {
+      throw new Error(e);
     }
   }
 }
 
-module.exports = ConceptsController
+module.exports = ConceptsController;
--- src/routes/index.js
+++ src/routes/index.js
@@ -6,7 +6,7 @@ const authMiddleware = require("../middlewares/auth");
 router.use("/api/auth", require("./auth"));
 router.use("/api/crops", authMiddleware.checkToken, require("./crop"));
 router.use("/api/fields", authMiddleware.checkToken, require("./field"));
-router.use("/api/concepts", authMiddleware.checkToken, require("./concept"));
+router.use("/api/concepts", authMiddleware.checkToken,require("./concept"));
 router.use("/api/lots", require("./lot"));
 router.use("/api/signs", authMiddleware.checkToken, require("./sign"));
 router.use("/api/files", require("./upload"))
