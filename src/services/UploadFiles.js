624ae4f7d50db62620467bd9eef021369eefc7c7
Franco Correa <anibalfranciscocorrea@gmail.com>
2019-07-12 19:08:32 -0300

    Se guarda los archivos con el nombre en md5

--- src/services/UploadFiles.js
+++ src/services/UploadFiles.js
@@ -14,19 +14,24 @@ class UploadFiles {
     if (!this.validTypes(this.files.file))
       throw new Error("File's extension is rejected");
 
+    //Ranem filename for md5 name file.
     let sampleFile = this.files.file;
+    const fileNameArray = sampleFile.name.split(".");
+    const renameFile = `${sampleFile.md5}.${fileNameArray[1]}`
+
+
     return new Promise((resolve, reject) => {
       sampleFile.mv(
         path.join(
           __dirname,
-          `../../public/${this.destination}/${sampleFile.name}`
+          `../../public/${this.destination}/${renameFile}`
         ),
         err => {
           if (err) reject(new Error("File's extension is rejected"));
 
           resolve({
             path: `${process.env.BASE_URL}/${this.destination}/${
-              sampleFile.name
+              renameFile
             }`
           });
         }
