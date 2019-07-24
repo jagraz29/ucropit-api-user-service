c2920c54bb254fb58fcc136fdcb9e98d7e6d77fd
Franco Correa <anibalfranciscocorrea@gmail.com>
2019-07-24 09:35:43 -0300

    Seeders user admin

---
+++ src/seeders/20190719134021-user_admin.js
@@ -0,0 +1,32 @@
+"use strict";
+
+const bcrypt = require("bcrypt");
+
+async function encryptPassword(password) {
+    const salt = await bcrypt.genSalt(10);
+    return await bcrypt.hash(password, salt);
+}
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return new Promise(async (resolve, reject) => {
+       return queryInterface.bulkInsert(
+        "users",
+        [
+          {
+            first_name: "John",
+            last_name: "Doe",
+            email: "admin@ucropit.com",
+            password: await encryptPassword("secret")
+          }
+        ],
+        {}
+      );
+    })
+    
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete("users", null, {});
+  }
+};
