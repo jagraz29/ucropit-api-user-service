568df6ac0015538de50226ea07b5d51570359291
Lucas Michailian <lucasmichailian@gmail.com>
2019-09-11 17:11:30 -0300

    change status when fields is done

--- src/controllers/ApprovalsRegisterController.js
+++ src/controllers/ApprovalsRegisterController.js
@@ -9,6 +9,9 @@ const ApprovalRegisterFiles = require('../models').approval_register_file
 const ProductionStages = require('../models').production_stage
 const Production = require('../models').productions
 const UploadFile = require("../services/UploadFiles");
+const Sequelize = require('sequelize')
+
+const Op = Sequelize.Op
 
 class ApprovalsRegisterController {
   static async complete(id) {
@@ -92,14 +95,22 @@ class ApprovalsRegisterController {
       where: { id: data.register_id }
     })
 
-    await Register.update({ data: JSON.stringify(data.meta) })
+    const register = await Register.update({ data: JSON.stringify(data.meta) })
 
-    return await ApprovalRegisterSigns.create({
+    await ApprovalRegisterSigns.create({
       approval_register_id: data.register_id,
       hash,
       ots: await Stamp.stampHash(hash),
       meta: JSON.stringify({ path }),
       user_id: auth.user.id
+    })
+
+    const production = await Production.findOne({ where: { crop_id: crop } })
+    const stages = await ProductionStages.update({ status: 'in_progress' }, {
+      where: {
+        production_id: production.id,
+        label: { [Op.not]: 'fields' }
+      }
     })
 
     return register
