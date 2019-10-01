044289dcdb777114b1c2aee664a62e51e6d6818d
Franco Correa <anibalfranciscocorrea@gmail.com>
2019-10-01 16:40:46 -0300

    Al enviar agenda de usuario, se filtra los usuarios que se registraron al sistema

--- src/controllers/DiaryUserController.js
+++ src/controllers/DiaryUserController.js
@@ -5,11 +5,14 @@ const DiaryUser = require("../models").diary_users;
 
 class DiaryUserController {
   static async index(auth) {
-    return await User.findAll(
+    return await User.findOne(
       {
         include: [
           {
             model: User,
+            where: {
+              first_login: 1
+            },
             as: "ContactUser",
             through: {
               model: DiaryUser
