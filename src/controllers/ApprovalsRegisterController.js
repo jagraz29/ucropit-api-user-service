78c9d1b885fba06e54d2a30ec3e6289cbd516515
Franco Correa <anibalfranciscocorrea@gmail.com>
2020-01-17 10:54:36 -0300

    Se agrega servicio para obtener los firmantes de un evento especifico

--- src/controllers/ApprovalsRegisterController.js
+++ src/controllers/ApprovalsRegisterController.js
@@ -1,121 +1,130 @@
-'use strict'
+"use strict";
 
-const PDF = require('../services/PDF')
-const Stamp = require('../services/Stamp')
-const User = require('../models').users
-const Approvals = require('../models').approval
-const ApprovalRegister = require('../models').approval_register
-const ApprovalRegisterSigns = require('../models').approval_register_sign
-const ApprovalRegisterFiles = require('../models').approval_register_file
-const ProductionStages = require('../models').production_stage
-const Production = require('../models').productions
-const UploadFile = require('../services/UploadFiles')
-const ExifFile = require('../services/ExifFile')
-const Sequelize = require('sequelize')
-const path = require('path')
+const PDF = require("../services/PDF");
+const Stamp = require("../services/Stamp");
+const User = require("../models").users;
+const Approvals = require("../models").approval;
+const ApprovalRegister = require("../models").approval_register;
+const ApprovalRegisterSigns = require("../models").approval_register_sign;
+const ApprovalRegisterFiles = require("../models").approval_register_file;
+const ProductionStages = require("../models").production_stage;
+const Production = require("../models").productions;
+const UploadFile = require("../services/UploadFiles");
+const ExifFile = require("../services/ExifFile");
+const Signers = require("../services/approvalRegisters/Signers");
+const Sequelize = require("sequelize");
+const path = require("path");
 
-const Sender = require('../services/approvalRegisters/Sender')
+const Sender = require("../services/approvalRegisters/Sender");
 
-const Op = Sequelize.Op
+const Op = Sequelize.Op;
 
 class ApprovalsRegisterController {
-  static async complete (id) {
-    const register = await Approvals.findOne({ where: { id } })
-    const type_id = register.service_id || register.input_id
+  static async complete(id) {
+    const register = await Approvals.findOne({ where: { id } });
+    const type_id = register.service_id || register.input_id;
 
     const production = await Production.findOne({
       where: { crop_id: register.crop_id },
       include: [
         {
           model: ProductionStages,
-          as: 'Stage',
+          as: "Stage",
           where: { label: register.stage }
         }
       ]
-    })
+    });
 
-    const stage = production.get({ plain: true }).Stage[0]
+    const stage = production.get({ plain: true }).Stage[0];
     const data = JSON.parse(stage.data).map(el => {
       if (el.field_id == type_id) {
-        el.status = 'done'
+        el.status = "done";
       }
-      return el
-    })
+      return el;
+    });
 
     const prodStage = await ProductionStages.findOne({
       where: { id: stage.id }
-    })
+    });
 
-    await prodStage.update({ data: JSON.stringify(data) })
+    await prodStage.update({ data: JSON.stringify(data) });
 
-    return prodStage
+    return prodStage;
   }
 
-  static async create (id) {
+  static async create(id) {
     return await ApprovalRegister.create({
       approval_id: id
-    })
+    });
   }
 
-  static async show (crop, stage, type, typeId) {
-    const approval =
-      stage === 'fields'
-        ? await Approvals.findOne({ where: { stage: stage, crop_id: crop } })
-        : await Approvals.findOne({
-            where: {
-              stage: stage,
-              crop_id: crop,
-              service_id: type === 'service' ? typeId : null,
-              input_id: type === 'input' ? typeId : null
-            }
-          })
+  static async show(cropId, stage, type, typeId) {
+    try {
+      const signers = await Signers.getSigners(stage, cropId,typeId, type);
 
-    const register = ApprovalRegister.findOne({
-      where: { approval_id: approval.id },
-      include: [
-        { model: ApprovalRegisterSigns, as: 'Signs' },
-        {
-          model: ApprovalRegisterFiles,
-          as: 'Files',
-          include: [{ model: User }]
-        }
-      ]
-    })
+      const approval =
+        stage === "fields"
+          ? await Approvals.findOne({
+              where: { stage: stage, crop_id: cropId }
+            })
+          : await Approvals.findOne({
+              where: {
+                stage: stage,
+                crop_id: cropId,
+                service_id: type === "service" ? typeId : null,
+                input_id: type === "input" ? typeId : null
+              }
+            });
 
-    return register
+      const register = await ApprovalRegister.findOne({
+        where: { approval_id: approval.id },
+        include: [
+          { model: ApprovalRegisterSigns, as: "Signs" },
+          {
+            model: ApprovalRegisterFiles,
+            as: "Files",
+            include: [{ model: User }]
+          }
+        ]
+      });
+
+      return { register, signers };
+    } catch (error) {
+      throw new Error(error);
+    }
   }
 
-  static async showRegister (id) {
+  static async showRegister(id) {
     const register = ApprovalRegister.findOne({
       where: { id },
       include: [
-        { model: ApprovalRegisterSigns, as: 'Signs' },
+        { model: ApprovalRegisterSigns, as: "Signs" },
         {
           model: ApprovalRegisterFiles,
-          as: 'Files',
+          as: "Files",
           include: [{ model: User }]
         }
       ]
-    })
+    });
 
-    return register
+    return register;
   }
 
-  static async sign (data, crop, stage, auth) {
+  static async sign(data, crop, stage, auth) {
     const { hash, path } = await PDF.generate({
       data: {
         time: new Date()
       },
-      template: 'templates/sign-receipt.pug',
+      template: "templates/sign-receipt.pug",
       path: `${__basedir}/../public/crop-${data.type_id}`,
       filename: `fields.pdf`
-    })
+    });
 
     const Register = await ApprovalRegister.findOne({
       where: { id: data.register_id }
-    })
+    });
 
-    const register = await Register.update({ data: JSON.stringify(data.meta) })
+    const register = await Register.update({ data: JSON.stringify(data.meta) });
 
     await ApprovalRegisterSigns.create({
       approval_register_id: data.register_id,
@@ -123,45 +132,50 @@ class ApprovalsRegisterController {
       ots: await Stamp.stampHash(hash),
       meta: JSON.stringify({ path }),
       user_id: auth.user.id
-    })
+    });
 
-    return register
+    return register;
   }
 
-  static async showFiles (idRegisterApproval) {
+  static async showFiles(idRegisterApproval) {
     const files = await ApprovalRegisterFiles.findAll({
       include: [{ model: User }],
       where: {
         approval_register_id: idRegisterApproval
       }
-    })
+    });
 
-    return files
+    return files;
   }
 
-  static async file (id, file, concept, auth, position = null, stage, pathname) {
+  static async file(id, file, concept, auth, position = null, stage, pathname) {
     try {
-      await Sender.needApprobation({ approvalRegister: id, stage, authUser: auth.user, pathname })
+      await Sender.needApprobation({
+        approvalRegister: id,
+        stage,
+        authUser: auth.user,
+        pathname
+      });
 
-      const upload = new UploadFile(file, 'uploads')
-      const res = await upload.store()
+      const upload = new UploadFile(file, "uploads");
+      const res = await upload.store();
 
       await ApprovalRegisterSigns.destroy({
         where: { approval_register_id: id }
-      })
+      });
 
-      if (res.fileType !== 'image/jpeg') {
+      if (res.fileType !== "image/jpeg") {
         return await ApprovalRegisterFiles.create({
           approval_register_id: id,
           concept,
           path: res.namefile,
           type: res.fileType
-        })
+        });
       }
 
       const exif = new ExifFile(
         path.join(__dirname, `../../public/uploads/${res.namefile}`)
-      )
+      );
 
       return await ApprovalRegisterFiles.create({
         approval_register_id: id,
@@ -171,11 +185,11 @@ class ApprovalsRegisterController {
         latitude: exif.metadata.tags.GPSLatitude || position.latitude,
         longitude: exif.metadata.tags.GPSLongitude || position.longitude,
         user_id: auth.user.id
-      })
+      });
     } catch (error) {
-      throw new Error(error)
+      throw new Error(error);
     }
   }
 }
 
-module.exports = ApprovalsRegisterController
+module.exports = ApprovalsRegisterController;
---
+++ src/services/approvalRegisters/Signers.js
@@ -0,0 +1,53 @@
+"use strict";
+
+const Common = require("./Common");
+
+class Signers {
+  /**
+   * Get all signers to event.
+   * 
+   * @param {*} stage 
+   * @param {*} cropId 
+   * @param {*} typeId 
+   * @param {*} type 
+   */
+  static async getSigners(stage, cropId, typeId, type) {
+    const permissions = await Common.getProductionPermisionsByCropId(cropId);
+
+    let canSignUsers = permissions
+      .map(el => {
+        return {
+          id: el.user_id,
+          events: el.permissions.events.find(el => stage === el.label),
+          stages: el.permissions.stages.find(el => stage === el.key)
+        };
+      })
+      .filter(el => el.events !== undefined);
+ 
+    if (stage !== "fields") {
+      canSignUsers = canSignUsers
+        .filter(el => el.events.events !== false)
+        .filter(el => {
+          if (Array.isArray(el.events.events)) {
+            return el.events.events.find(el => {
+              return (
+                el.field_id == typeId &&
+                el.type === type &&
+                el.permissions.can_sign === true
+              );
+            });
+          } else {
+            return el;
+          }
+        });
+    } else {
+      canSignUsers = canSignUsers.filter(el => {
+        return el.stages.permissions.can_sign === true;
+      });
+    }
+    
+    return canSignUsers;
+  }
+}
+
+module.exports = Signers;
