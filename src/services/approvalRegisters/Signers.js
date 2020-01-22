ef0df6d0d56029d42b2c407992e4a99791de62de
Lucas Michailian <lucasmichailian@gmail.com>
2020-01-22 11:49:26 -0300

    fix show up

--- src/services/approvalRegisters/Signers.js
+++ src/services/approvalRegisters/Signers.js
@@ -1,6 +1,8 @@
-"use strict";
+'use strict'
 
-const Common = require("./Common");
+const Common = require('./Common')
+
+const TOTAL_STAGES = 8
 
 class Signers {
   /**
@@ -12,42 +14,43 @@ class Signers {
    * @param {*} type
    */
   static async getSigners(stage, cropId, typeId, type) {
-    const permissions = await Common.getProductionPermisionsByCropId(cropId);
+    const permissions = await Common.getProductionPermisionsByCropId(cropId)
+    let aux = []
+    let canSignUsers = permissions.map((el) => {
+      return {
+        id: el.user_id,
+        events: el.permissions.events.find((el) => stage === el.label),
+        stages: el.permissions.stages.find((el) => stage === el.key),
+        allStages: el.permissions.stages.filter((el) => el.permissions.can_sign === true).length === TOTAL_STAGES
+      }
+    }).filter((el) => el.events !== undefined)
+    
+    aux = canSignUsers.filter(el => el.allStages)
 
-    let canSignUsers = permissions
-      .map(el => {
-        return {
-          id: el.user_id,
-          events: el.permissions.events.find(el => stage === el.label),
-          stages: el.permissions.stages.find(el => stage === el.key)
-        };
-      })
-      .filter(el => el.events !== undefined);
-
-    if (stage !== "fields") {
+    if (stage !== 'fields') {
       canSignUsers = canSignUsers
-        .filter(el => el.events.events !== false)
-        .filter(el => {
+        .filter((el) => el.events.events !== false)
+        .filter((el) => {
           if (Array.isArray(el.events.events)) {
-            return el.events.events.find(el => {
+            return el.events.events.find((el) => {
               return (
                 el.field_id == typeId &&
                 el.type === type &&
                 el.permissions.can_sign === true
-              );
-            });
+              )
+            })
           } else {
-            return el;
+            return el
           }
-        });
+        })
     } else {
-      canSignUsers = canSignUsers.filter(el => {
-        return el.stages.permissions.can_sign === true;
-      });
+      canSignUsers = canSignUsers.filter((el) => {
+        return el.stages.permissions.can_sign === true
+      })
     }
 
-    return canSignUsers;
+    return [...canSignUsers, aux]
   }
 }
 
-module.exports = Signers;
+module.exports = Signers
