62421fb00b3e7de62f73b2eedf5420779ebc4b8e
Lucas Michailian <lucasmichailian@gmail.com>
2020-03-19 08:54:20 -0300

    publish changes

--- src/controllers/ConceptsController.js
+++ src/controllers/ConceptsController.js
@@ -1,124 +1,125 @@
-"use strict";
+'use strict'
 
-const Services = require("../models").services;
-const Inputs = require("../models").inputs;
-const InputType = require("../models").input_types;
-const ServiceType = require("../models").service_types;
-const Sequelize = require("sequelize");
-const { paginate } = require("../helpers");
+const Services = require('../models').services
+const Inputs = require('../models').inputs
+const InputType = require('../models').input_types
+const ServiceType = require('../models').service_types
+const Sequelize = require('sequelize')
+const { paginate } = require('../helpers')
 
 class ConceptsController {
   static async index() {
     try {
-      const inputs = await Inputs.findAll({ include: [{ model: InputType }] });
+      const inputs = await Inputs.findAll({ include: [{ model: InputType }] })
       const services = await Services.findAll({
         include: [{ model: ServiceType }]
-      });
+      })
 
       return Promise.resolve([
-        ...services.map(el => {
-          const plain = el.get({ plain: true });
-          plain.category = "service";
-          return plain;
+        ...services.map((el) => {
+          const plain = el.get({ plain: true })
+          plain.category = 'service'
+          return plain
         }),
-        ...inputs.map(el => {
-          const plain = el.get({ plain: true });
-          plain.category = "input";
-          return plain;
+        ...inputs.map((el) => {
+          const plain = el.get({ plain: true })
+          plain.category = 'input'
+          return plain
         })
-      ]);
+      ])
     } catch (e) {
-      console.erro(e);
-      throw new Error(e);
+      console.erro(e)
+      throw new Error(e)
     }
   }
 
   static async show(id, type) {
     try {
-      if (type === "service") {
+      if (type === 'service') {
         const input = await Services.findOne({
           where: { id },
           include: [{ model: ServiceType }]
-        });
-        return input;
+        })
+        return input
       } else {
         const services = await Inputs.findOne({
           where: { id },
           include: [{ model: InputType }]
-        });
-        return services;
+        })
+        return services
       }
     } catch (e) {
-      console.erro(e);
-      throw new Error(e);
+      console.erro(e)
+      throw new Error(e)
     }
   }
 
   static async store(data) {
     try {
-      const input = await Inputs.create({ ...data, stage: "all" });
+      const input = await Inputs.create({ ...data, stage: 'all' })
 
-      return input;
+      return input
     } catch (e) {
-      console.erro(e);
-      throw new Error(e);
+      console.erro(e)
+      throw new Error(e)
     }
   }
 
   static async update(id, data) {
     try {
-      const input = await Inputs.findOne({ where: { id: id } });
-      return await input.update(data);
+      const input = await Inputs.findOne({ where: { id: id } })
+      return await input.update(data)
     } catch (e) {
-      console.error(e);
-      throw new Error(e);
+      console.error(e)
+      throw new Error(e)
     }
   }
 
   static async delete(id) {
     try {
-      const input = await Inputs.findOne({ where: { id: id } });
-      return await input.destroy();
+      const input = await Inputs.findOne({ where: { id: id } })
+      return await input.destroy()
     } catch (e) {
-      console.error(e);
-      throw new Error(e);
+      console.error(e)
+      throw new Error(e)
     }
   }
 
   static async inputTypes(query) {
     try {
-      const results = await InputType.findAll();
+      const results = await InputType.findAll()
 
-      return results;
+      return results
     } catch (e) {
-      console.erro(e);
-      throw new Error(e);
+      console.erro(e)
+      throw new Error(e)
     }
   }
 
   static async inputTypesWithAttribute() {
     try {
       const results = await InputType.findAll({
-        attributes: ["id", "name"]
-      });
+        attributes: ['id', 'name']
+      })
 
-      return results;
+      return results
     } catch (e) {
-      console.erro(e);
-      throw new Error(e);
+      console.erro(e)
+      throw new Error(e)
     }
   }
 
   static async input() {
     try {
       const results = await Inputs.findAll({
+        order: [['name', 'ASC']],
         include: [{ model: InputType }]
-      });
+      })
 
-      return results;
+      return results
     } catch (e) {
-      console.erro(e);
-      throw new Error(e);
+      console.error(e)
+      throw new Error(e)
     }
   }
 
@@ -130,7 +131,7 @@ class ConceptsController {
         },
         { page, pageSize }
       )
-    );
+    )
 
     return {
       total: Math.ceil(count / pageSize),
@@ -138,22 +139,22 @@ class ConceptsController {
       currentPage: page,
       lastPage: Math.ceil(count / pageSize - 1 + 1),
       data: inputs
-    };
+    }
   }
 
   static async searchInpuType(query) {
-    const Op = Sequelize.Op;
+    const Op = Sequelize.Op
     try {
       const results = await InputType.findAll({
         where: { name: { [Op.like]: `%${query}%` } }
-      });
+      })
 
-      return results;
+      return results
     } catch (e) {
-      console.erro(e);
-      throw new Error(e);
+      console.erro(e)
+      throw new Error(e)
     }
   }
 }
 
-module.exports = ConceptsController;
+module.exports = ConceptsController
