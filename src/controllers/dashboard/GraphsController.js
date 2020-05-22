1fe681ec7ca92e2f6b56f3b4c50e42a6aac106ad
Lucas Michailian <lucasmichailian@gmail.com>
2020-05-22 14:13:00 -0300

    opts

--- src/controllers/dashboard/GraphsController.js
+++ src/controllers/dashboard/GraphsController.js
@@ -1,27 +1,38 @@
 'use static'
 
 const _ = require('lodash')
-const Common = require('../../services/approvalRegisters/Common')
+const ProductionPermission = require('../../services/production/ProductionPermissions')
 const CropService = require('../../services/dashboard/crops/CropService')
 const CropTypes = require('../../models').crop_types
 
 const SINGNED_OPTS = {
-  label: 'Firmadas',
+  label: 'Firmado',
   backgroundColor: '#5FD856',
   stack: '1',
 }
 
 const REGISTERED_OPTS = {
-  label: 'A Registrar',
-  backgroundColor: '#ccc',
+  label: 'Registrado',
+  backgroundColor: '#FFD101',
   stack: '1',
 }
+
+const TO_REGISTER_OPTS = {
+  label: 'A Registrar',
+  backgroundColor: '#ccc',
+  stack: '2',
+}
+
 
 class GraphsController {
   static async surfacePerCrop(req, res) {
     const registeredSurfaces = await CropService.getCropRegisteredSurfacesBy({
       company: req.params.companyId,
     })
+
+    console.log(
+      await ProductionPermission.whoCanSign('field')
+    )
 
     res.json({
       labels: registeredSurfaces.map((el) => el.name),
@@ -31,7 +42,7 @@ class GraphsController {
           data: [30, 50, 20, 40],
         },
         {
-          ...REGISTERED_OPTS,
+          ...TO_REGISTER_OPTS,
           data: registeredSurfaces.map((el) => el.surface),
         },
       ],
