661fa461e520af243863910dab6bb57ad376ae14
Franco Correa <anibalfranciscocorrea@gmail.com>
2020-05-26 08:39:10 -0300

    Se agrega calculo para obtener porcentaje de avaneces por clientes, grafico de barras

--- src/controllers/dashboard/GraphsController.js
+++ src/controllers/dashboard/GraphsController.js
@@ -3,6 +3,8 @@
 const _ = require('lodash')
 const Common = require('../../services/approvalRegisters/Common')
 const CropService = require('../../services/dashboard/crops/CropService')
+const CompanyService = require('../../services/dashboard/company/CompanyService')
+const SignService = require('../../services/dashboard/signs/SignService')
 const CropTypes = require('../../models').crop_types
 
 const SINGNED_OPTS = {
@@ -36,6 +38,41 @@ class GraphsController {
         },
       ],
     })
+  }
+
+  static async percentSignature(companyId) {
+    try {
+      const customers = await CompanyService.getCompaniesProductors(companyId)
+
+      const labels = customers.map(company => {
+        return company.name
+      })
+
+      console.log(labels)
+
+      SignService.summary(customers)
+
+      // {
+      //   labels: ['Algodón', 'Soja', 'Maiz', 'Girasol'],
+      //   datasets: [
+      //     {
+      //       label: 'Firmadas',
+      //       backgroundColor: '#5FD856',
+      //       stack: '1',
+      //       data: [30, 50, 20, 40, 50, 30, 20, 110]
+      //     },
+      //     {
+      //       label: 'Registradas',
+      //       backgroundColor: '#ccc',
+      //       stack: '2',
+      //       data: [10, 0, 5, 15, 0, 4, 8, 8]
+      //     }
+      //   ]
+      // }
+    }catch(error) {
+      console.log(error)
+      throw new Error(error)
+    }
   }
 }
 
