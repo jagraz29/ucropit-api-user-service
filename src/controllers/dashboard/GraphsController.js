c8bf2968d7ce9e9c89341b70c4490261236bbaa4
Franco Correa <anibalfranciscocorrea@gmail.com>
2020-05-26 14:51:34 -0300

    Estructura para representar porcentajes de registros y registros firmados

--- src/controllers/dashboard/GraphsController.js
+++ src/controllers/dashboard/GraphsController.js
@@ -1,11 +1,7 @@
 'use static'
-
-const _ = require('lodash')
-const ProductionPermission = require('../../services/production/ProductionPermissions')
 const CropService = require('../../services/dashboard/crops/CropService')
 const CompanyService = require('../../services/dashboard/company/CompanyService')
 const SignService = require('../../services/dashboard/signs/SignService')
-const CropTypes = require('../../models').crop_types
 
 const SINGNED_OPTS = {
   label: 'Firmado',
@@ -50,31 +46,36 @@ class GraphsController {
     try {
       const customers = await CompanyService.getCompaniesProductors(companyId)
 
+      const progressRegister = await SignService.summaryRegister(customers)
+      const progressSign = await SignService.summarySigned(customers)
+
       const labels = customers.map(company => {
         return company.name
       })
 
-      console.log(labels)
+      const dataSigns = progressSign.map(result => {
+        return result.totalSigns[0]
+      })
 
-      SignService.summary(customers)
+      const dataRegister = progressRegister.map(result => {
+        return result.totalRegister[0]
+      })
 
-      // {
-      //   labels: ['Algodón', 'Soja', 'Maiz', 'Girasol'],
-      //   datasets: [
-      //     {
-      //       label: 'Firmadas',
-      //       backgroundColor: '#5FD856',
-      //       stack: '1',
-      //       data: [30, 50, 20, 40, 50, 30, 20, 110]
-      //     },
-      //     {
-      //       label: 'Registradas',
-      //       backgroundColor: '#ccc',
-      //       stack: '2',
-      //       data: [10, 0, 5, 15, 0, 4, 8, 8]
-      //     }
-      //   ]
-      // }
+
+      return {
+        labels: labels,
+        datasets: [
+          {
+            ...SINGNED_OPTS,
+            data: dataSigns
+          },
+          {
+            ...REGISTERED_OPTS,
+            stack: '2',
+            data: dataRegister
+          }
+        ]
+      }
     }catch(error) {
       console.log(error)
       throw new Error(error)
--- src/routes/dashboard/graphs.js
+++ src/routes/dashboard/graphs.js
@@ -7,4 +7,15 @@ router.get('/surfacePerCrop/:companyId', (req, res) => {
   return GraphsController.surfacePerCrop(req, res)
 })
 
+router.get('/signatures/comapies/:companyId', (req, res) => {
+  const { companyId } = req.params
+  GraphsController.percentSignature(companyId)
+    .then((result) => {
+      res.status(200).json({ result, error: false })
+    })
+    .catch((error) => {
+      res.status(500).json({ error: true, message: error })
+    })
+})
+
 module.exports = router
