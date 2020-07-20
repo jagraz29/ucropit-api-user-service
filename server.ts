799e922e40c06ec887fb5b17e5c4f1331ef71eeb
Lucas Michailian <lucasmichailian@gmail.com>
2020-07-20 15:54:45 -0300

    recomendated extensions file

--- server.ts
+++ server.ts
@@ -1,15 +1,17 @@
+require('dotenv').config()
+
 import express = require('express')
 import swaggerUI from 'swagger-ui-express'
 import { swaggerDocs } from './config/swagger'
- 
 import routes from './src/routes/v1'
 
+const port = process.env.NODE_PORT || 3000
 const app: express.Application = express()
 
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs))
 
 app.use('/v1', routes)
 
-app.listen(3000, function () {
-  console.log('App is listening on port 3000!')
+app.listen(port, function () {
+  console.log(`App is listening on port ${port}!`)
 })
