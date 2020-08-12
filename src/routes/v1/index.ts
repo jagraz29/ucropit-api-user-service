d510f4357007e15ded54bad15a312dcafc5406b0
Franco Correa <anibalfranciscocorrea@gmail.com>
2020-08-12 09:55:16 -0300

    FileUpload services

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,16 +1,19 @@
-import express from 'express'
-import 'express-async-errors'
-import users from './users'
-import auth from './auth'
+import express from "express";
+import "express-async-errors";
+import users from "./users";
+import auth from "./auth";
+import test from "./test";
 
-const router: express.Router = express.Router()
+const router: express.Router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send('v1 APP OK')
-})
+router.get("/", (req, res) => {
+  res.send("v1 APP OK");
+});
 
-router.use('/auth', auth)
+router.use("/auth", auth);
 
-router.use('/users', users)
+router.use("/users", users);
 
-export default router
+router.use("/test", test);
+
+export default router;
---
+++ src/services/FileUpload.ts
@@ -0,0 +1,54 @@
+import { FileArray, UploadedFile } from "express-fileupload";
+import path from "path";
+
+class FileUpload {
+  files: FileArray;
+  destination: string;
+
+  constructor(files: FileArray, destination: string) {
+    this.files = files;
+    this.destination = destination;
+  }
+
+  store() {
+    if (Object.keys(this.files).length === 0) {
+      throw new Error("No files were uploaded.");
+    }
+
+    if (!this.validTypes(this.files.files)) {
+      throw new Error("File extension is rejected");
+    }
+
+    const sampleFile: UploadedFile = this.files.files;
+
+    const fileNameArray = sampleFile.name.trim().split(".");
+
+    const renameFile = `${sampleFile.md5}.${fileNameArray.pop()}`;
+
+    return new Promise((resolve, reject) => {
+      sampleFile.mv(
+        path.join(process.cwd(), `/public/${this.destination}/${renameFile}`),
+        (err) => {
+          console.log(err);
+          if (err) reject(new Error("File's extension is rejected"));
+
+          resolve({
+            path: `${process.env.BASE_URL}/${this.destination}/${renameFile}`,
+            namefile: renameFile,
+            fileType: sampleFile.mimetype,
+          });
+        }
+      );
+    });
+  }
+
+  validTypes(file) {
+    return (
+      file.mimetype.match(
+        "text.*|image.*|application/pdf|application/msword|application/vnd.openxmlformats-officedocument.wordprocessingml.document|application/octet-stream|application/vnd.google-earth.kmz|application/vnd.google-earth.kml"
+      ) !== null
+    );
+  }
+}
+
+export default FileUpload;
