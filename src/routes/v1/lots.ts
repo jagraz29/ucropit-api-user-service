dd66454703eb8267e58dce0ed0b334671e2f1a7a
Franco Correa <anibalfranciscocorrea@gmail.com>
2020-08-12 15:11:23 -0300

    Api doc endpoint get surfaces

--- src/routes/v1/lots.ts
+++ src/routes/v1/lots.ts
@@ -4,6 +4,44 @@ import lotController from '../../controllers/LotController'
 
 const router: express.Router = express.Router()
 
+/**
+ * @swagger
+ * path:
+ *  /v1/lots/surfaces:
+ *    post:
+ *      summary: Get names of areas by given kmz/kml file
+ *      tags: [Lots]
+ *      requestBody:
+ *        content:
+ *          multipart/form-data:
+ *              schema:
+ *               type: object
+ *               properties:
+ *                  files:
+ *                       type: string
+ *                       format: binary
+ *      responses:
+ *       '200':
+ *         description: List names and areas to lost kmz or kml file.
+ *         content:
+ *          application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 name:
+ *                   type: string
+ *                   description: Name of lot.
+ *                 surface:
+ *                   type: string
+ *                   description: Surface of lot.
+ *                 area:
+ *                   type: double
+ *                   description: Area of lot.
+ *
+ *       '500':
+ *         description: Error to Server.
+ *
+ */
 router.post('/surfaces', lotController.surfaces)
 
 export default router
