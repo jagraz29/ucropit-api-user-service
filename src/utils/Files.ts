5dfc1149d50047fd20c0c079cf18202f1c064e16
Franco Correa <anibalfranciscocorrea@gmail.com>
2020-08-28 10:22:32 -0300

    Helper files method

---
+++ src/utils/Files.ts
@@ -0,0 +1,32 @@
+const mkdirp = require('mkdirp')
+import { join } from 'path'
+
+import * as fs from 'fs'
+
+export async function makeDirIfNotExists (dir) {
+  await mkdirp(dir)
+  return dir
+}
+
+export function getFullPath (filePath: string) {
+  return join(publicPath(), filePath)
+}
+
+function publicPath () {
+  return join(basePath(), `public`)
+}
+
+function basePath (): string {
+  return join(__dirname, '../')
+}
+
+export function removeFile (dir: string) {
+  fs.unlinkSync(dir)
+}
+
+export function fileExist (dir: string) {
+  if (fs.existsSync(dir)) {
+    return true
+  }
+  return false
+}
