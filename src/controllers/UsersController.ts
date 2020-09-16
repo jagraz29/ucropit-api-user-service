0d890b31a8fab6ccfade0265e4308de3ebcb36e7
Lucas Michailian <lucasmichailian@gmail.com>
2020-09-16 20:25:38 -0300

    put profile image

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -1,42 +1,20 @@
 import { Request, Response } from 'express'
 import models from '../models'
-import UserService from '../services/UserService'
 
 const User = models.User
 
 class UsersController {
-  public async index (req: Request, res: Response) {
+  /**
+   * Store profile image.
+   *
+   * @param req
+   * @param res
+   *
+   * @return Response
+   */
+  public async image (req: Request, res: Response) {
     const users = await User.find({})
-    res.status(200).json({ users })
-  }
-
-  public async show (req: Request, res: Response) {
-    const user = await User.findById(req.params.id)
-    res.json({ user })
-  }
-
-  public async create (req: Request, res: Response) {
-    const user = await UserService.store(req.body)
-    res.json(user)
-  }
-
-  public async update (req: Request, res: Response) {
-    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
-      new: true
-    })
-
-    if (!user) {
-      res.status(404).json({ error: 'ERR_NOT_FOUND' })
-    } else {
-      res.json(user)
-    }
-  }
-
-  public async destroy (req: Request, res: Response) {
-    const user = await User.findByIdAndDelete(req.params.id)
-    res.json({
-      message: 'deleted successfully'
-    })
+    res.status(200).json(users)
   }
 }
 
