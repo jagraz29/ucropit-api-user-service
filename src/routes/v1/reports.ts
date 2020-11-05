ace584c320405bdeb6942b85ade22770c30f90f0
Franco Correa <anibalfranciscocorrea@gmail.com>
2020-11-05 13:15:45 -0300

    Integrate basic auth for download report

--- src/routes/v1/reports.ts
+++ src/routes/v1/reports.ts
@@ -1,10 +1,11 @@
 import express from 'express'
 
 import reportsController from '../../controllers/ReportsController'
+import { checkAuth } from '../../utils/auth/BasicAuth'
 
 const router: express.Router = express.Router()
 
-router.get('/crops', reportsController.generateCrops)
+router.get('/crops', checkAuth, reportsController.generateCrops)
 
 router.get('/map/lot', reportsController.showMap)
 
