9fec841e2752f55b89816a06b778833fe589072a
Franco Correa <anibalfranciscocorrea@gmail.com>
2020-11-05 13:25:16 -0300

    Add docuemnt report

--- src/routes/v1/reports.ts
+++ src/routes/v1/reports.ts
@@ -5,6 +5,25 @@ import { checkAuth } from '../../utils/auth/BasicAuth'
 
 const router: express.Router = express.Router()
 
+/**
+ * @swagger
+ * path:
+ *  /v1/reports/crops:
+ *    get:
+ *      summary: Download reports
+ *      tags: [Reports]
+ *      responses:
+ *        "200":
+ *          description: Show success
+ *          content:
+ *            application/json:
+ *             schema:
+ *                $ref: '#/components/schemas/Crop'
+ *        "404":
+ *          description: Not Found Resources
+ *        "500":
+ *          description: Server error
+ */
 router.get('/crops', checkAuth, reportsController.generateCrops)
 
 router.get('/map/lot', reportsController.showMap)
