cadf51fcd484dcc44e63b4c94dc915ac677b97f9
Franco Correa <anibalfranciscocorrea@gmail.com>
2020-11-26 09:43:31 -0300

    Add new attribute in fileDocument

--- src/models/documentFile.ts
+++ src/models/documentFile.ts
@@ -47,6 +47,9 @@ export const FileDocumentSchema = new Schema({
   settings: {
     type: Schema.Types.Mixed
   },
+  meta: {
+    type: Schema.Types.Mixed
+  },
   user: {
     type: Schema.Types.ObjectId,
     ref: 'User'
