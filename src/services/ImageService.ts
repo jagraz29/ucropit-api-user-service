b83d9e895d568403e2cfd98ab5d928d10c82116d
Franco Correa <anibalfranciscocorrea@gmail.com>
2020-12-14 10:13:20 -0300

    Service to manipulate image

--- src/services/ImageService.ts
+++ src/services/ImageService.ts
@@ -1,21 +1,47 @@
 import sharp from 'sharp'
+import { getFullPath } from '../utils/Files'
 
 interface ResizeImageParameters {
   path: string
   destination: string
-  width: Number
-  height: Number
+  nameFile: string
+  suffixName?: string
+  width?: Number
+  height?: Number
 }
+
 class ImageService {
   /**
    * Resize Image
    *
    * @param ResizeImageParameters parameters
    */
-  public async resize (parameters: ResizeImageParameters) {
-    console.log('Parametros')
-    console.log(parameters)
+  public static async resize (parameters: ResizeImageParameters) {
+    const pathImage = `${parameters.destination}/${parameters.suffixName}-${parameters.nameFile}`
+    await sharp(getFullPath(parameters.path))
+      .resize(parameters.width, parameters.height)
+      .toFile(getFullPath(pathImage))
+
+    return pathImage
+  }
+
+  /**
+   * Create Thumbnail image.
+   *
+   * @param parameters
+   */
+  public static async createThumbnail (parameters: ResizeImageParameters) {
+    const pathImageThumbnails = await this.resize({
+      path: parameters.path,
+      destination: parameters.destination,
+      nameFile: parameters.nameFile,
+      suffixName: 'thumbnail',
+      width: 200,
+      height: 200
+    })
+
+    return pathImageThumbnails
   }
 }
 
-export default new ImageService()
+export default ImageService
