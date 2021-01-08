79ae25853487f779d7caf2f69525129b3b1047d5
Franco Correa <anibalfranciscocorrea@gmail.com>
2021-01-08 12:31:00 -0300

    Update configuration user and selected company in array companies

--- src/routes/v1/configs.ts
+++ src/routes/v1/configs.ts
@@ -5,7 +5,7 @@ const router: express.Router = express.Router()
 /**
  * @swagger
  * path:
- *  /v1/configurations/:id:
+ *  /v1/configurations/{id}:
  *    put:
  *      summary: Update configuration
  *      tags: [Configs]
@@ -37,5 +37,44 @@ const router: express.Router = express.Router()
  *
  */
 router.put('/:id', configsController.update)
+
+/**
+ * @swagger
+ * path:
+ *  /v1/configurations/{id}/companies:
+ *    put:
+ *      summary: Update configuration and selected company into companies user.
+ *      tags: [Configs]
+ *      parameters:
+ *        - in: path
+ *          name: id
+ *          required: true
+ *        - in: query
+ *          name: identifier
+ *          required: true
+ *      requestBody:
+ *        content:
+ *          application/json:
+ *              schema:
+ *               type: object
+ *               properties:
+ *                  companySelected:
+ *                    type: string
+ *                    required: true
+ *                    description: ID de la compañia seleccionada
+ *
+ *      responses:
+ *       '200':
+ *         description: Update UserConfig.
+ *         content:
+ *            application/json:
+ *             schema:
+ *                $ref: '#/components/schemas/UserConfig'
+ *
+ *       '500':
+ *         description: Error to Server.
+ *
+ */
+router.put('/:id/companies', configsController.updateSelectedCompany)
 
 export default router
