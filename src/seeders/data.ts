58d6b30ed9994c02121667d7bfa88d8131774ba7
Fatima Palacios <maria1990palacios@gmail.com>
2021-02-10 16:22:38 -0400

    fixed language error in provider seeder

--- src/seeders/data.ts
+++ src/seeders/data.ts
@@ -323,8 +323,8 @@ export const rolesData = [
   },
   {
     label: {
-      en: 'Proveedor',
-      es: 'Provider',
+      en: 'Provider',
+      es: 'Proveedor',
     },
     value: 'PROVIDER'
   },
