ae6861c7bf149016ed214bb0a24b90d00211a52b
Franco Correa <anibalfranciscocorrea@gmail.com>
2021-03-31 12:20:22 -0300

    Control duplicate name lots when create crops

--- src/types/common.ts
+++ src/types/common.ts
@@ -3,5 +3,6 @@ export const roles = ['MARKETER', 'PROVIDER', 'CAM']
 export const errors = [
   { key: '001', code: 'NOT_AUTHORIZATION_EXPORT' },
   { key: '002', code: 'ERROR_KMZ_INVALID_FORMAT' },
-  { key: '003', code: 'ERROR_FILE_EXTENSION' }
+  { key: '003', code: 'ERROR_FILE_EXTENSION' },
+  { key: '004', code: 'NAME_LOT_DUPLICATED' }
 ]
