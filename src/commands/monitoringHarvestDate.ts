f508bda6e716214636a939bc3feb73e02528d5cb
Franco Correa <anibalfranciscocorrea@gmail.com>
2021-04-01 10:22:01 -0300

    Command add harvest date estimated

--- src/commands/monitoringHarvestDate.ts
+++ src/commands/monitoringHarvestDate.ts
@@ -2,46 +2,82 @@ require('dotenv').config()
 import models, { connectDb } from '../models'
 import chalk from 'chalk'
 import { flatten } from 'lodash'
-import BlockChainServices from '../services/BlockChainService'
-import ApprovalRegisterSingService from '../services/ApprovalRegisterSignService'
+import CropService from '../services/CropService'
 
 const Crop = models.Crop
 
 const addHarvestDateMonitoring = async () => {
-  let crops = await Crop.find({ cancelled: false })
-    .populate('cropType')
-    .populate({
-      path: 'done',
-      populate: [{ path: 'type' }]
-    })
-    .populate({
-      path: 'finished',
-      populate: [{ path: 'type' }]
-    })
+  try {
+    let crops = await Crop.find({ cancelled: false })
+      .populate('cropType')
+      .populate({
+        path: 'pending',
+        populate: [{ path: 'type' }]
+      })
+      .populate({
+        path: 'done',
+        populate: [{ path: 'type' }]
+      })
+      .populate({
+        path: 'finished',
+        populate: [{ path: 'type' }]
+      })
 
-  crops = crops.filter(
-    (crop) => crop.finished.length > 0 || crop.done.length > 0
-  )
+    crops = crops.filter(
+      (crop) => crop.finished.length > 0 || crop.done.length > 0
+    )
 
-  const activities = crops
-    .map((crop) => {
-      const activitiesDone = crop.done.filter(
-        (activity) => activity.type.tag === 'ACT_MONITORING'
-      )
-      const activitiesFinished = crop.finished.filter(
-        (activity) => activity.type.tag === 'ACT_MONITORING'
-      )
+    const activities = crops
+      .map((crop) => {
+        const activitiesPending = crop.pending.filter(
+          (activity) => activity.type.tag === 'ACT_MONITORING'
+        )
+        const activitiesDone = crop.done.filter(
+          (activity) => activity.type.tag === 'ACT_MONITORING'
+        )
+        const activitiesFinished = crop.finished.filter(
+          (activity) => activity.type.tag === 'ACT_MONITORING'
+        )
 
-      return {
-        activities: [...activitiesDone, ...activitiesFinished],
-        cropId: crop._id
+        return {
+          activities: [
+            ...activitiesPending,
+            ...activitiesDone,
+            ...activitiesFinished
+          ],
+          crop: crop
+        }
+      })
+      .filter((activity) => activity.activities.length > 0)
+
+    const flatActivities = flatten(activities)
+
+    for (const item of flatActivities) {
+      for (const activity of item.activities) {
+        if (!activity.dateEstimatedHarvest) {
+          const monitoring = await CropService.getLastMonitoring(item.crop._id)
+          activity.dateEstimatedHarvest =
+            monitoring.dateEstimatedHarvest || item.crop.dateHarvest
+
+          await activity.save()
+
+          console.log(
+            `${chalk.green(
+              `Activity ID: ${activity._id} is updated with dateEstimatedHarvest`
+            )}`
+          )
+        } else {
+          console.log(
+            `${chalk.green(
+              `Activity ID: ${activity._id} is already has dateEstimatedHarvest`
+            )}`
+          )
+        }
       }
-    })
-    .filter((activity) => activity.activities.length > 0)
-
-  console.log('ACTIVIDADES')
-  const flatActivities = flatten(activities)
-  console.log(flatActivities)
+    }
+  } catch (error) {
+    console.log(`${chalk.red(`Error: ${error}`)}`)
+  }
 }
 
 ;(async () => {
