cd6b9c5c87c5d55d3bef23f79329b783d36c6717
Franco Correa <anibalfranciscocorrea@gmail.com>
2021-04-26 17:40:10 -0300

    Documentation webhooks in swagger

--- src/routes/v1/webhooks.ts
+++ src/routes/v1/webhooks.ts
@@ -5,15 +5,47 @@ const router: express.Router = express.Router()
 /**
  * @swagger
  * path:
- *  /v1/offline/crops?identifier={identifier}:
+ *  /v1/webhooks/sensing/callback:
  *    post:
  *      summary: Reset sync of crop
- *      tags: [Offline]
+ *      tags: [WebHooks]
+ *      requestBody:
+ *        content:
+ *          application/json:
+ *              schema:
+ *               type: object
+ *               properties:
+ *                  status_ok:
+ *                    type: boolean
+ *                    description: Status Response
+ *                  lotId:
+ *                    type: string
+ *                    description: ID Lot
+ *                  customOptions:
+ *                    type: object
+ *                    required: false
+ *                    properties:
+ *                          activityId:
+ *                            type: string
+ *                            description: ID Activity
+ *                  images:
+ *                     type: array
+ *                     items:
+ *                          type: object
+ *                          properties:
+ *                              nameFile:
+ *                                type: string
+ *                                description: Name file satellite image
+ *                              date:
+ *                                 type: string
+ *                                 format: date
+ *                                 description: 2020-05-01
+ *                              type:
+ *                                 type: string
+ *                                 description: RGB or NDVI
  *      responses:
  *        "200":
- *          description: Show success
- *        "404":
- *          description: Not Found Resources
+ *          description: Show Ok
  *        "500":
  *          description: Server error
  */
