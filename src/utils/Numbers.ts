c342aeee4af28028d63e3eb05c8170bd56ff2e4b
Franco Correa <anibalfranciscocorrea@gmail.com>
2021-05-18 11:10:11 -0300

    Fix import Number in ChartController

--- src/utils/Numbers.ts
+++ src/utils/Numbers.ts
@@ -1,14 +1,12 @@
 export class Numbers {
-  public static getRandom (size = 6) {
+  public static getRandom(size = 6) {
     return Math.floor(
       Math.pow(10, size - 1) +
         Math.random() * (Math.pow(10, size) - Math.pow(10, size - 1) - 1)
     )
   }
 
-  public static roundToTwo (num: number) {
+  public static roundToTwo(num: number) {
     return Math.round((num + Number.EPSILON) * 100) / 100
   }
 }
-
-// export Numbers
