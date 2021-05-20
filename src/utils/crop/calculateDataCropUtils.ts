ffbddc3929ea04eda55714c7d97c14b04822a6e9
ygarcia2 <ygarcia@shokworks.io>
2021-05-20 09:08:35 -0400

    AP-362

--- src/utils/crop/calculateDataCropUtils.ts
+++ src/utils/crop/calculateDataCropUtils.ts
@@ -1,12 +1,30 @@
 import util from 'util'
+import { Numbers } from '../Numbers'
+import { calculateCropVolumeUtils } from './calculateCropVolumeUtils'
 
-export const calculateDataCropUtils = ({ surface, pay, dateCrop, name, activities, lots }): Object => {
+export const calculateDataCropUtils = ({
+    surface,
+    pay: payEntry,
+    dateCrop,
+    name,
+    activities,
+    lots,
+    company,
+    unitType,
+    cropType: { key: cropTypeKey }
+  }): Object => {
+  const pay = payEntry ?? 0
+  const { key: keyUnitType, name: nameUnitType } = unitType || {}
   // console.log(util.inspect(lots, { showHidden: false, depth: null }))
   return {
     surface,
-    volume: surface * (pay ? pay : 0),
+    volume: Numbers.roundToTwo(
+      calculateCropVolumeUtils(keyUnitType, pay, surface)
+    ),
     pay,
     dateCrop,
-    name
+    name,
+    cropTypeKey,
+    company
   }
 }
