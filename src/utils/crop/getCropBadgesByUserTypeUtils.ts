87dfa660d3a52fe32b0d6e604dd19a2817a545c6
Franco Correa <anibalfranciscocorrea@gmail.com>
2021-05-27 23:15:03 -0300

    Add Type User Auth in method getCropBadgesByUserTypeUtils

--- src/utils/crop/getCropBadgesByUserTypeUtils.ts
+++ src/utils/crop/getCropBadgesByUserTypeUtils.ts
@@ -1,23 +1,33 @@
-import { UserTypes } from '../../interfaces'
+import { UserAuth, UserTypes } from '../../interfaces'
 
-export const getCropBadgesByUserType = ({ _id }, { badges, members }) => {
+export const getCropBadgesByUserType = (
+  { _id }: UserAuth,
+  { badges, members }
+) => {
   /*
   GET MEMBER DATA OF USER IN CROP
   */
-  const memberData = members.filter((member) => _id.toString() === member.user._id.toString())[0]
+  const memberData = members.filter(
+    (member) => _id.toString() === member.user._id.toString()
+  )[0]
 
   /*
   VALIDATE USER TYPE
   */
-  if(memberData.type !== UserTypes.MARKETER && memberData.type !== UserTypes.PROVIDER) return badges
+  if (
+    memberData.type !== UserTypes.MARKETER &&
+    memberData.type !== UserTypes.PROVIDER
+  ) {
+    return badges
+  }
 
   /*
   RETURN ONLY BADGES ALLOWED FOR THAT USER TYPE
   */
   return badges.filter((badge) => {
-    if(!badge.typeAgreement?.visible) return false
+    if (!badge.typeAgreement?.visible) return false
 
-    if(badge.typeAgreement.visible.includes(memberData.identifier)) return true
+    if (badge.typeAgreement.visible.includes(memberData.identifier)) return true
 
     return false
   })
