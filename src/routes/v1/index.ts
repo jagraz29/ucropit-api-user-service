3336b0d2de8ecbbbf0711337f5958b4411b76735
Edwar Susarra <anthwam13@outlook.com>
2021-05-31 01:06:28 -0400

    fix: restructuring the pdf

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,97 +1,97 @@
-import express, { Response } from 'express';
-import 'express-async-errors';
-import users from './users';
-import profile from './profile';
-import auth from './auth';
-import lots from './lots';
-import crop from './crop';
-import cropCollaborators from './cropCollaborators';
-import activities from './activities';
-import company from './company';
-import common from './common';
-import configs from './configs';
-import passport from '../../utils/auth/strategies/jwt';
-import achievements from './achievements';
-import collaborators from './collaborators';
-import reports from './reports';
-import charts from './charts';
-import badges from './badges';
-import files from './files';
-import supplies from './supplies';
-import offline from './offline';
-import integrations from './integrations';
-import webhooks from './webhooks';
+import express, { Response } from 'express'
+import 'express-async-errors'
+import users from './users'
+import profile from './profile'
+import auth from './auth'
+import lots from './lots'
+import crop from './crop'
+import cropCollaborators from './cropCollaborators'
+import activities from './activities'
+import company from './company'
+import common from './common'
+import configs from './configs'
+import passport from '../../utils/auth/strategies/jwt'
+import achievements from './achievements'
+import collaborators from './collaborators'
+import reports from './reports'
+import charts from './charts'
+import badges from './badges'
+import files from './files'
+import supplies from './supplies'
+import offline from './offline'
+import integrations from './integrations'
+import webhooks from './webhooks'
 
-const router: express.Router = express.Router();
+const router: express.Router = express.Router()
 
 router.get('/', (req, res) => {
-  res.send('v1 APP OK');
-});
+  res.send('v1 APP OK')
+})
 
-const authMiddleware = passport.authenticate('jwt', { session: false });
+const authMiddleware = passport.authenticate('jwt', { session: false })
 
 router.get('/fast-links', (req, res: Response) => {
-  res.render('fast-links', { url: process.env.SCHEMA_URL });
-});
+  res.render('fast-links', { url: process.env.SCHEMA_URL })
+})
 
 // AUTH
-router.use('/auth', auth);
+router.use('/auth', auth)
 
 // USERS
-router.use('/users', authMiddleware, users);
+router.use('/users', authMiddleware, users)
 
 // PROFILE
-router.use('/profile', authMiddleware, profile);
+router.use('/profile', authMiddleware, profile)
 
 // LOTS
-router.use('/lots', authMiddleware, lots);
+router.use('/lots', authMiddleware, lots)
 
 // COMMON
-router.use('/commons', authMiddleware, common);
+router.use('/commons', authMiddleware, common)
 
 // CROPS
-router.use('/crops', authMiddleware, crop);
+router.use('/crops', authMiddleware, crop)
 
 // CROPS
-router.use('/crops', authMiddleware, cropCollaborators);
+router.use('/crops', authMiddleware, cropCollaborators)
 
 // ACTIVITIES
-router.use('/activities', authMiddleware, activities);
+router.use('/activities', authMiddleware, activities)
 
 // COMPANIES
-router.use('/companies', authMiddleware, company);
+router.use('/companies', authMiddleware, company)
 
 // CONFIGURATIONS
-router.use('/configurations', authMiddleware, configs);
+router.use('/configurations', authMiddleware, configs)
 
 // ACHIEVEMENTS
-router.use('/achievements', authMiddleware, achievements);
+router.use('/achievements', authMiddleware, achievements)
 
 // COLLABORATOR
-router.use('/collaborators', authMiddleware, collaborators);
+router.use('/collaborators', authMiddleware, collaborators)
 
 // CHARTS CROPS
-router.use('/charts', authMiddleware, charts);
+router.use('/charts', authMiddleware, charts)
 
 // BADGES
-router.use('/badges', authMiddleware, badges);
+router.use('/badges', authMiddleware, badges)
 
 // REPORTS
-router.use('/reports', reports);
+router.use('/reports', reports)
 
 // FILE DOCUMENTS
-router.use('/files', files);
+router.use('/files', files)
 
 // SUPPLIES
-router.use('/supplies', supplies);
+router.use('/supplies', supplies)
 
 // OFFLINE
-router.use('/offline', authMiddleware, offline);
+router.use('/offline', authMiddleware, offline)
 
 // API EXPORTER
-router.use('/exporters', authMiddleware, integrations);
+router.use('/exporters', authMiddleware, integrations)
 
 // WEBHOOKS SERVICES
-router.use('/webhooks', webhooks);
+router.use('/webhooks', webhooks)
 
-export default router;
+export default router
--- src/services/PDFService.ts
+++ src/services/PDFService.ts
@@ -1,16 +1,11 @@
-import { v4 as uuidv4 } from 'uuid';
-import Handlebars from 'handlebars';
-import Puppeteer from 'puppeteer';
-import sha256 from 'sha256';
-import {
-  makeDirIfNotExists,
-  readFileBytes,
-  saveFile,
-  readFile,
-} from '../utils';
-import { FileDocumentRepository } from '../repositories';
-import { setScriptPdf } from '../helpers';
-import { FileDocumentProps } from '../interfaces';
+import { v4 as uuidv4 } from 'uuid'
+import Handlebars from 'handlebars'
+import Puppeteer from 'puppeteer'
+import sha256 from 'sha256'
+import { makeDirIfNotExists, readFileBytes, saveFile, readFile } from '../utils'
+import { FileDocumentRepository } from '../repositories'
+import { setScriptPdf } from '../helpers'
+import { FileDocumentProps } from '../interfaces'
 
 export class PDFService {
   public static async generatePdf(
@@ -21,25 +16,25 @@ export class PDFService {
     { _id: cropId }
   ): Promise<string | null> {
     const fileDocuments: FileDocumentProps | null =
-      await FileDocumentRepository.getFiles(cropId);
+      await FileDocumentRepository.getFiles(cropId)
 
-    const path: string = `public/uploads/${directory}/`;
-    await makeDirIfNotExists(path);
-    const fullName: string = `${nameFile}-${uuidv4()}.pdf`;
-    const pathFile: string = `${path}${fullName}`;
+    const path: string = `public/uploads/${directory}/`
+    await makeDirIfNotExists(path)
+    const fullName: string = `${nameFile}-${uuidv4()}.pdf`
+    const pathFile: string = `${path}${fullName}`
 
-    const hbs: string = readFile(`views/pdf/html/${nameTemplate}.hbs`);
-    const handlebarsWithScript = setScriptPdf(Handlebars);
-    const template = handlebarsWithScript.compile(hbs);
-    const html = template(context, 'utf-8');
-    saveFile(`public/uploads/${directory}/content.html`, html);
+    const hbs: string = readFile(`views/pdf/html/${nameTemplate}.hbs`)
+    const handlebarsWithScript = setScriptPdf(Handlebars)
+    const template = handlebarsWithScript.compile(hbs)
+    const html = template(context, 'utf-8')
+    saveFile(`public/uploads/${directory}/content.html`, html)
     // console.log(html);
 
-    const browser = await Puppeteer.launch();
-    const page = await browser.newPage();
-    await page.setContent(html);
-    await page.addStyleTag({ path: `views/pdf/css/${nameTemplate}.css` });
-    await page.emulateMediaType('screen');
+    const browser = await Puppeteer.launch()
+    const page = await browser.newPage()
+    await page.setContent(html)
+    await page.addStyleTag({ path: `views/pdf/css/${nameTemplate}.css` })
+    await page.emulateMediaType('screen')
     const pdfBytes = await page.pdf({
       format: 'A4',
       printBackground: true,
@@ -49,17 +44,17 @@ export class PDFService {
         right: '20px',
         bottom: '20px',
       },
-    });
+    })
 
     if (!fileDocuments) {
-      saveFile(pathFile, pdfBytes);
+      saveFile(pathFile, pdfBytes)
       await FileDocumentRepository.createFile({
         nameFile: fullName,
         path: pathFile,
         date: new Date(),
         cropId,
-      });
-      return fullName;
+      })
+      return fullName
     }
 
     return this.findAndSavePdfExists(
@@ -68,7 +63,7 @@ export class PDFService {
       fileDocuments,
       cropId,
       pdfBytes
-    );
+    )
   }
 
   private static async findAndSavePdfExists(
@@ -78,21 +73,21 @@ export class PDFService {
     cropId,
     pdfBytes
   ) {
-    const pathFile = `public/uploads/${directory}/`;
-    const oldPdfBytes = readFileBytes(`${pathFile}${nameFile}`);
+    const pathFile = `public/uploads/${directory}/`
+    const oldPdfBytes = readFileBytes(`${pathFile}${nameFile}`)
     if (oldPdfBytes) {
       // console.log(sha256(pdfBytes),sha256(oldPdfBytes))
       if (sha256(pdfBytes) === sha256(oldPdfBytes)) {
-        return nameFile;
+        return nameFile
       }
     }
-    saveFile(`${pathFile}${fullName}`, pdfBytes);
+    saveFile(`${pathFile}${fullName}`, pdfBytes)
     await FileDocumentRepository.createFile({
       nameFile: fullName,
       path: `${pathFile}${fullName}`,
       date: new Date(),
       cropId,
-    });
-    return fullName;
+    })
+    return fullName
   }
 }
