c7ee78d2f91ee3de10c45e07faff56b80f9f810a
Franco Correa <anibalfranciscocorrea@gmail.com>
2021-05-31 19:35:54 -0300

    Fix path image lot

--- src/utils/lots/getLotsUtils.ts
+++ src/utils/lots/getLotsUtils.ts
@@ -1,22 +1,24 @@
-import { Numbers } from '../Numbers';
-import { getEiqOfAchievementsByLot } from '.';
+import { Numbers } from '../Numbers'
+import { getEiqOfAchievementsByLot } from '.'
 
 export const getLots = (lots, activitiesWithEiq): Object[] => {
   return lots.map(
     ({ provinceName, _id: lotId, cityName, countryName, surface, image }) => {
-      const { normal: path } = image || {};
+      const { normal: path } = image || {}
       const imageLot = path
-        ? `${process.env.BASE_URL}${process.env.DIR_STORAGE}${path}`
+        ? `${process.env.BASE_URL}${path}`
         : process.env.IMAGE_LOT_DEFAULT
-      const eiq: number = getEiqOfAchievementsByLot(lotId, activitiesWithEiq);
+
+      console.log(imageLot)
+      const eiq: number = getEiqOfAchievementsByLot(lotId, activitiesWithEiq)
       return {
         provinceName,
         cityName,
         countryName,
         surface,
         eiq: Numbers.roundToTwo(eiq),
-        image: imageLot,
-      };
+        image: imageLot
+      }
     }
-  );
-};
+  )
+}
