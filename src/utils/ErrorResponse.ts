b3f08e726731ec4d087c8ebe675bf0ebb5d2d8e7
Arquimedes Rodriguez <arqui78@gmail.comm>
2021-06-01 14:06:50 -0400

    chare: Clase para gestinar los erros en un estandar de apiRestFull

---
+++ src/utils/ErrorResponse.ts
@@ -0,0 +1,42 @@
+import { StatusCodes } from 'http-status-codes'
+
+export type TError = {
+    code: string
+    message: string
+    description?: string,
+    errors?: Error[]
+}
+
+interface IErrorResponse {
+    parseStatusCode (statusCode: StatusCodes): number
+
+    // tslint:disable-next-line:unified-signatures
+    parseError (code: string, message: string, description?: string, errors?: Error[]): TError,
+}
+
+class ErrorResponse implements IErrorResponse {
+
+    public static ERROR_SERVER = 'ERROR_SERVER'
+    public static NOT_AUTHORIZATION_EXPORT = 'NOT_AUTHORIZATION_EXPORT'
+    public static ERROR_KMZ_INVALID_FORMAT = 'ERROR_KMZ_INVALID_FORMAT'
+    public static ERROR_FILE_EXTENSION = 'ERROR_FILE_EXTENSION'
+    public static NAME_LOT_DUPLICATED = 'NAME_LOT_DUPLICATED'
+    public static RESOURCE_NOT_FOUND = 'RESOURCE_NOT_FOUND'
+    public static BADGE_TYPE_DUPLICATED = 'BADGE_TYPE_DUPLICATED'
+    public static DATA_NOT_FOUND = 'DATA_NOT_FOUND'
+
+    parseError (code: string, message: string, description?: string, errors?: Error[]): TError {
+        return {
+            code,
+            message,
+            description,
+            errors
+        }
+    }
+
+    parseStatusCode (statusCode: StatusCodes): number {
+        return /^2/.test(statusCode.toString()) ? 500 : statusCode
+    }
+}
+export const ErrorResponseInstance = new ErrorResponse()
+export default ErrorResponse
