24e7e68a681c9da24499a7ccd0fa2e5e5f4716f6
ygarcia2 <ygarcia@shokworks.io>
2021-06-01 16:19:59 -0400

    AP-455, Finish bug

--- src/services/PDFService.ts
+++ src/services/PDFService.ts
@@ -1,24 +1,22 @@
 import { v4 as uuidv4 } from 'uuid'
 import Handlebars from 'handlebars'
+import pdfParse from 'pdf-parse'
 import Puppeteer from 'puppeteer'
-import sha256 from 'sha256'
-import { makeDirIfNotExists, readFileBytes, saveFile, readFile } from '../utils'
+import { makeDirIfNotExists, readFileBuffer, saveFile, readFile, basePath } from '../utils'
 import { FileDocumentRepository } from '../repositories'
 import { setScriptPdf } from '../helpers'
 import { FileDocumentProps } from '../interfaces'
 
 export class PDFService {
-  public static async generatePdf(
+  public static async generatePdf (
     nameTemplate: string,
     context: object,
-    directory: string,
+    nameDirectory: string,
     nameFile: string,
     { _id: cropId }
-  ): Promise<string | null> {
-    const fileDocuments: FileDocumentProps | null =
-      await FileDocumentRepository.getFiles(cropId)
-
-    const path: string = `public/uploads/${directory}/`
+  ): Promise<string> {
+    const fileDocuments: Array<FileDocumentProps> | null = await FileDocumentRepository.getFiles(cropId)
+    const path: string = `public/uploads/${nameDirectory}/`
     await makeDirIfNotExists(path)
     const fullName: string = `${nameFile}-${uuidv4()}.pdf`
     const pathFile: string = `${path}${fullName}`
@@ -28,13 +26,13 @@ export class PDFService {
     const template = handlebarsWithScript.compile(hbs)
     const html = template(context)
     // saveFile(`public/uploads/${directory}/content.html`, html);
-    console.log(html)
+    // console.log(html)
 
     const browser = await Puppeteer.launch()
     const page = await browser.newPage()
     await page.setContent(html)
     await page.addStyleTag({ path: `views/pdf/css/${nameTemplate}.css` })
-    const pdfBytes = await page.pdf({
+    const pdfNewBuffer = await page.pdf({
       format: 'A4',
       printBackground: true,
       margin: {
@@ -46,7 +44,7 @@ export class PDFService {
     })
 
     if (!fileDocuments) {
-      saveFile(pathFile, pdfBytes)
+      saveFile(pathFile, pdfNewBuffer)
       await FileDocumentRepository.createFile({
         nameFile: fullName,
         path: pathFile,
@@ -57,33 +55,41 @@ export class PDFService {
     }
 
     return this.findAndSavePdfExists(
-      directory,
+      nameDirectory,
       fullName,
       fileDocuments,
       cropId,
-      pdfBytes
+      pdfNewBuffer
     )
   }
 
-  private static async findAndSavePdfExists(
+  private static async findAndSavePdfExists (
     directory: string,
     fullName: string,
-    { nameFile }: FileDocumentProps,
+    fileDocuments: FileDocumentProps[],
     cropId,
-    pdfBytes
+    pdfNewBuffer
   ) {
-    const pathFile = `public/uploads/${directory}/`
-    const oldPdfBytes = readFileBytes(`${pathFile}${nameFile}`)
-    if (oldPdfBytes) {
-      // console.log(sha256(pdfBytes),sha256(oldPdfBytes))
-      if (sha256(pdfBytes) === sha256(oldPdfBytes)) {
-        return nameFile
+    const { text: textNewPdf } = await pdfParse(pdfNewBuffer)
+    const pathFile = `public/uploads/${directory}/${fullName}`
+
+    const fileDocument = await Promise.all(fileDocuments.map(async (fileDocument) => {
+      const oldPdfBuffer = readFileBuffer(fileDocument.path)
+      if (oldPdfBuffer) {
+        const { text: textOldPdf } = await pdfParse(oldPdfBuffer)
+        if (textNewPdf === textOldPdf) return fileDocument
+        return null
       }
+    }))
+
+    if (fileDocument[0]) {
+      return fileDocument[0].nameFile
     }
-    saveFile(`${pathFile}${fullName}`, pdfBytes)
+
+    saveFile(pathFile, pdfNewBuffer)
     await FileDocumentRepository.createFile({
       nameFile: fullName,
-      path: `${pathFile}${fullName}`,
+      path: pathFile,
       date: new Date(),
       cropId
     })
