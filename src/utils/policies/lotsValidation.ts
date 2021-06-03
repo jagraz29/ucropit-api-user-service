6bced6e8aa9da7448735e7fdf112a21f690d61fe
Arquimedes Rodriguez <arqui78@gmail.comm>
2021-06-03 09:18:06 -0400

    fix: Setiando mensaje de error segun el campo que sea requerido

--- src/utils/policies/lotsValidation.ts
+++ src/utils/policies/lotsValidation.ts
@@ -3,6 +3,10 @@ import { ReasonPhrases, StatusCodes } from 'http-status-codes'
 import * as Joi from 'joi'
 import ErrorResponse, { ErrorResponseInstance } from '../ErrorResponse'
 
+const messages = {
+  identifier: 'El CUIT es requerido para continuar con la operación',
+  dateCrop: 'Debe ingresar una fecha en el valor ‘Fecha de cultivo’.'
+}
 export const getLotsPolicy = (
   req: Request | any,
   res: Response,
@@ -16,7 +20,12 @@ export const getLotsPolicy = (
   const { error } = schema.validate(req.query)
 
   if (error) {
-    return res.status(StatusCodes.UNPROCESSABLE_ENTITY).json(ErrorResponseInstance.parseError(ErrorResponse.REQUIRED_FIELDS, ReasonPhrases.BAD_REQUEST, {error}))
+    let field = error.details[0].path[0]
+
+    return res.status(StatusCodes.UNPROCESSABLE_ENTITY).json(ErrorResponseInstance.parseError(ErrorResponse.REQUIRED_FIELDS, messages[field], {
+      description: ReasonPhrases.BAD_REQUEST,
+      error
+    }))
   }
 
   return next()
