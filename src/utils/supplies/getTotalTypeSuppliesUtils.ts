b416746bb9b2ee4fd0da6b522c3d5d616472e194
ygarcia2 <ygarcia@shokworks.io>
2021-06-03 15:32:47 -0400

    AP-247, EIQ PRODUCTS Fix

--- src/utils/supplies/getTotalTypeSuppliesUtils.ts
+++ src/utils/supplies/getTotalTypeSuppliesUtils.ts
@@ -12,14 +12,15 @@ export const getTotalTypeSupplies = (supplies): Object => {
   let totalTypeSupplies = []
   for (const key in SupplyType) {
     let count = 0
-    if (supplies.length) {
-      supplies.map(({ type: { code, name } }) => {
+    supplies.map((supply) => {
+      if (supply.type) {
+        const { code, name } = supply.type
         if (code === key) {
           totalTypeSupplies.push({ quantity: count + 1, name })
         }
-      })
-    }
+      }
 
+    })
   }
   return totalTypeSupplies
 }
