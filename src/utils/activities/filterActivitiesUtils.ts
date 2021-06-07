ba154556d1e87ca2af0ee8c09f57ad024ad77a8c
Franco Correa <anibalfranciscocorrea@gmail.com>
2021-06-07 18:25:15 -0300

    filter activities

--- src/utils/activities/filterActivitiesUtils.ts
+++ src/utils/activities/filterActivitiesUtils.ts
@@ -2,30 +2,7 @@ export const filterActivities = (activities) => {
   return activities.filter(filterByActivityMakeAndFinished)
 }
 export const filterByActivityMakeAndFinished = (activity) => {
-  if (
-    (activity.status === 'REALIZADA' || activity.status === 'TERMINADA') &&
-    (activity.tag === 'ACT_HARVEST' ||
-      activity.tag === 'ACT_MONITORING' ||
-      activity.tag === 'ACT_AGREEMENT') &&
-    isCompleteSignersSign(activity.signers)
-  ) {
+  if (activity.status === 'TERMINADA' || activity.status === 'REALIZADA') {
     return activity
   }
-
-  if (
-    activity.achievements.length &&
-    (activity.status === 'TERMINADA' || activity.status === 'REALIZADA')
-  ) {
-    return activity
-  }
-}
-
-const isCompleteSignersSign = (signers) => {
-  const listNotSigners = signers.filter((item) => !item.signed)
-
-  if (listNotSigners.length) {
-    return false
-  }
-
-  return true
 }
