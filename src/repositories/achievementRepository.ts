82fd55962f80f10ad2cb953298ab2116bd5a6616
ygarcia2 <ygarcia@shokworks.io>
2021-06-08 01:12:19 -0400

    Refactor models

--- src/repositories/achievementRepository.ts
+++ src/repositories/achievementRepository.ts
@@ -1,5 +1,5 @@
 import { AchievementModel } from '../models'
-import { IAchievementDocument } from '../interfaces'
+import { IAchievementDocument, IAchievement } from '../interfaces'
 
 export class AchievementRepository {
   /**
@@ -7,7 +7,7 @@ export class AchievementRepository {
    * @param update
    * @param string id
    */
-  static async updateAchievement(update: Partial<IAchievementDocument>, id: string): Promise<IAchievementDocument | any > {
+  static async updateAchievement(update: Partial<IAchievement>, id: string): Promise<IAchievementDocument | any > {
     return AchievementModel.updateOne({ _id: id }, { $set: update })
   }
 }
