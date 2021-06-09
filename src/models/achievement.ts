56227868874db0519bdeb839bffd629919308038
ygarcia2 <ygarcia@shokworks.io>
2021-06-09 10:24:35 -0400

    Refactor models add routes V2 the crop with lots

--- src/models/achievement.ts
+++ src/models/achievement.ts
@@ -3,13 +3,14 @@ import shortid from 'shortid'
 import { IAchievementDocument } from '../interfaces'
 import { AchievementSchema } from '../schemas'
 
-AchievementSchema.pre('save', async (next: Function) => {
-  const achievement = this
+AchievementSchema.pre('save',  (next: Function) => {
+  const achievement: any = this
 
   /** Generate unique key */
   if (!achievement.key) {
     achievement.key = shortid.generate()
   }
+  next()
 })
 
 export const AchievementModel = model<IAchievementDocument>('Achievement', AchievementSchema)
