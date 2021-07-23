cbd6c801003fb56e12862561016d9bb797e514aa
Yurian Garcia <yurians19@gmail.com>
2021-07-23 09:30:57 -0400

    fix: error no return res in pin

--- src/utils/auth/managementTokenJwtPin.ts
+++ src/utils/auth/managementTokenJwtPin.ts
@@ -4,7 +4,9 @@ export const generateAuthTokenPin = (id): string => {
   return jwt.sign({ id }, process.env.AUTH_KEY_PIN_JWT, { expiresIn: '1h' })
 }
 
-export const checkTokenPin = (tokenParam: string): Boolean => {
+export const checkTokenPin = async (
+  tokenParam: string
+): Promise<void | boolean> => {
   let token = ''
   if (tokenParam && tokenParam.startsWith('Bearer ')) {
     token = tokenParam.slice(7, tokenParam.length)
@@ -14,11 +16,15 @@ export const checkTokenPin = (tokenParam: string): Boolean => {
     return false
   }
   if (token) {
-    jwt.verify(token, process.env.AUTH_KEY_PIN_JWT, async (err, decoded) => {
-      if (err) {
-        return false
+    return jwt.verify(
+      token,
+      process.env.AUTH_KEY_PIN_JWT,
+      async (err, decoded) => {
+        if (err) {
+          return false
+        }
+        return true
       }
-      return true
-    })
+    )
   }
 }
