c0a8e2bff916f0460ab59b9beed7f42d52dabc43
Arquimedes Rodriguez <arqui78@gmail.com>
2021-07-28 19:06:37 -0400

    chore: setting query dept in use case

--- src/core/cropTypes/useCase/getCropType/getCropTypesUseCase.ts
+++ src/core/cropTypes/useCase/getCropType/getCropTypesUseCase.ts
@@ -1,10 +1,11 @@
 import CropTypeRepo from '../../repo/cropTypeRepo'
 import { ICropTypeProps } from '../../interfaces'
+import { GetCropTypesDTO } from './getCropTypesDTO'
 
 export default class GetCropTypesUseCase {
   constructor(private cropTypeRepo: CropTypeRepo) {}
 
-  public execute(query: object = {}): Promise<ICropTypeProps[]> {
+  public execute(query: GetCropTypesDTO = {}): Promise<ICropTypeProps[]> {
     return this.cropTypeRepo.get(query)
   }
 }
