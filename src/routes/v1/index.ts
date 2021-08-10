eec96ad0a0854ae34257945ffafa792d02d664fb
Franco Correa <anibalfranciscocorrea@gmail.com>
2021-08-10 14:41:54 -0300

    feat: add endpoint ping pong response

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -37,6 +37,10 @@ router.get('/fast-links', (req, res: Response) => {
   res.render('fast-links', { url: process.env.SCHEMA_URL })
 })
 
+router.get('/ping', (req: Request, res: Response) => {
+  res.send('pong')
+})
+
 // AUTH
 router.use('/auth', auth)
 
