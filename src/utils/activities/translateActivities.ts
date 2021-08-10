5d0901803b57bb7e4ec1b6a8918dd9d4ab88f305
Franco Correa <anibalfranciscocorrea@gmail.com>
2021-08-10 19:23:59 -0300

    fix: add translate status activity

--- src/utils/activities/translateActivities.ts
+++ src/utils/activities/translateActivities.ts
@@ -5,14 +5,17 @@ export const translateActivities = (activities: Activity[], lang: string) => {
   setLocale(lang)
   const translated = activities.map((activity) => {
     const tag: string = activity.type?.tag.toLowerCase()
+    const status: string = activity.status[0]?.name?.en.toLowerCase()
     const typeAgreementKey: string = activity.typeAgreement
       ? activity.typeAgreement.key.toLowerCase()
       : null
     const translatedName: string = __(`activity_types.tag.${tag}`)
+    const translateStatus: string = __(`activity_status.status.${status}`)
 
     return {
       ...activity,
       name: translatedName,
+      statusLabel: translateStatus,
       typeAgreement: {
         ...activity.typeAgreement,
         translatedName: typeAgreementKey
