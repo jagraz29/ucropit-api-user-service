2320493721484ba76f22e02d97304e7cb56ded93
Yurian Garcia <yurians19@gmail.com>
2021-08-11 11:14:19 -0400

    fix: add interface subTypeActivity

--- src/models/subTypeActivity.ts
+++ src/models/subTypeActivity.ts
@@ -1,8 +1,8 @@
 import { model } from 'mongoose'
-import { IEiqRanges } from '../interfaces'
+import { ISubTypeActivity } from '../interfaces'
 import { SubTypeActivitySchema } from '../schemas'
 
-export const SubTypeActivityModel = model<IEiqRanges>(
+export const SubTypeActivityModel = model<ISubTypeActivity>(
   'EiqRanges',
   SubTypeActivitySchema
 )
