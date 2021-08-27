11a176eed7a6d0a0281ccd04a6b9726f36b45709
Arquimedes Rodriguez <arqui78@gmail.com>
2021-08-27 18:46:45 -0400

    fix: repairing activities and cropTypes in supplytypes

--- src/commands/updateActivityTypeInSupplyType.ts
+++ src/commands/updateActivityTypeInSupplyType.ts
@@ -4,6 +4,7 @@ import { SupplyTypeDocument } from '../models/supplyType'
 import chalk from 'chalk'
 import { Command, OptionValues } from 'commander'
 import { typesSupplies } from '../utils/Constants'
+import { uniq } from 'lodash'
 
 import { SupplyTypeRepository } from '../repositories'
 
@@ -16,18 +17,27 @@ program
 program.parse(process.argv)
 
 const updateSupplyTypeWithActivityType = async (): Promise<void> => {
+  //Fix Code SeSo en Semilla Sorgo por SeSor
+  const query = { name: 'Semilla de Sorgo' }
+  const supplyType = await SupplyTypeRepository.findOneByQuery(query)
+  if (supplyType) {
+    await SupplyTypeRepository.updateOne(supplyType._id, { code: 'SeSor' })
+  }
+
   for (const item of typesSupplies) {
     const supplyTypes: SupplyTypeDocument[] =
-      await SupplyTypeRepository.getByIds(item.types)
+      await SupplyTypeRepository.getByCodes(item.codes)
 
     if (supplyTypes.length) {
       for (const supplyType of supplyTypes) {
-        const activities = supplyType.activities
-        activities.push(item.tag)
-        await SupplyTypeRepository.updateOne(supplyType._id, { activities })
+        const activities = supplyType.activities ?? []
+        if (item.tag) activities.push(item.tag)
+        await SupplyTypeRepository.updateOne(supplyType._id, {
+          activities: uniq(activities)
+        })
         console.log(
           `${chalk.green(
-            `SupplyType ID: ${supplyType._id} is already has updated`
+            `SupplyType ID: ${supplyType._id} | code: ${supplyType.code} is already has updated`
           )}`
         )
       }
--- src/repositories/supplyTypeRepository.ts
+++ src/repositories/supplyTypeRepository.ts
@@ -5,6 +5,7 @@ const { SupplyType } = models
 export class SupplyTypeRepository {
   /**
    * Get all supply types
+   * @function getAll
    * @returns
    */
   public static async getAll(): Promise<SupplyTypeDocument[]> {
@@ -12,16 +13,28 @@ export class SupplyTypeRepository {
   }
 
   /**
-   *
+   * @function getAllByQuery
    * @param query
    * @returns
    */
   public static async getAllByQuery(query: any): Promise<SupplyTypeDocument[]> {
     return SupplyType.find(query).lean()
   }
+
+  /**
+   * @function findOneByQuery
+   * @param query
+   * @returns
+   */
+  public static async findOneByQuery(
+    query: any
+  ): Promise<SupplyTypeDocument[]> {
+    if (Object.keys(query).length === 0) return null
+    return SupplyType.findOne(query).lean()
+  }
   /**
    * Get SupplyType by ids.
-   *
+   * @function getByIds
    * @param ids
    * @returns
    */
@@ -30,7 +43,19 @@ export class SupplyTypeRepository {
   }
 
   /**
-   *Update One SupplyType
+   * Get SupplyType by codes.
+   * @function getByCodes
+   * @param codes
+   * @returns
+   */
+  public static async getByCodes(
+    codes: String[]
+  ): Promise<SupplyTypeDocument[]> {
+    return SupplyType.find({ code: { $in: codes } })
+  }
+
+  /**
+   * Update One SupplyType
    * @param id
    * @param queryUpdate
    */
